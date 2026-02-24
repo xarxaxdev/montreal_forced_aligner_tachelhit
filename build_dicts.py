aa0ff80d615f59e68212a0200694660625cb22f0
xarxaxdev <xarxax.dev@gmail.com>
2026-02-25 00:33:03 +0100

    aligning transliteration between latin and tifinagh

--- build_dicts.py
+++ build_dicts.py
@@ -62,12 +62,16 @@ https://medium.com/@evan.frank/accessing-and-cleaning-bulk-wikipedia-text-data-b
 # https://ieeexplore.ieee.org/abstract/document/8284715
 # SOURCE 5
 # https://commons.wikimedia.org/wiki/Tifinagh
+
+# SOURCE 6 (for specific berber variants)
+#https://en.wikipedia.org/wiki/Berber_Latin_alphabet
 tifinagh2ipa_dict = {
     ### VOWELS AND GLIDES
     #'ⴰ':'æ',# SRC 1
     'ⴰ':'a',# SRC 2,3,4
     #'ⴻ':'ə',# SRC 1  CONFLICT!
-    'ⴻ':'e',# SRC 2,3,4  CONFLICT!
+    'ⴻ':'e',# SRC 2,3,4; in normal Berber is 'ə' but not in Tachelhit
+    #https://en.wikipedia.org/wiki/Berber_Latin_alphabet#Souss-Berber_local_usage
     'ⵉ':'i',# CONSENSUS SRC 1,2,3,4
     #'ⵄ':'ʕ',# SRC 1  CONFLICT!
     'ⵄ':'ɛ',# SRC 2,3,4  CONFLICT!
@@ -87,9 +91,11 @@ tifinagh2ipa_dict = {
 
     # Bilabials
     'ⴱ':'b',# CONSENSUS 1,2,3,4
+    'ⴱⵯ':'bʷ',# SRC 6; velarization
     'ⴲ':'β',# IRCAM EXTENDED fricative; SRC 1,2
     'ⵒ':'p', # IRCAM EXTENDED; SRC 1,2
     'ⵎ':'m',# CONSENSUS 1,2,3,4
+    'ⵎⵯ':'mʷ',# SRC 6;velarization
 
     # Labiodental
     'ⴼ':'f',# CONSENSUS 1,2,3,4
@@ -103,15 +109,17 @@ tifinagh2ipa_dict = {
  
     # Alveolar
     'ⵏ':'n',# CONSENSUS 1,2,3,4
+    
     'ⵙ':'s',# CONSENSUS 1,2,3,4
     'ⵚ':'sˤ',# CONSENSUS 1,2,3,4
     'ⵣ':'z',# CONSENSUS 1,2,3,4
     'ⵥ':'zˤ',# CONSENSUS 1,2,3,4
+    
     'ⵜ':'t',# CONSENSUS 1,2,3,4
     'ⵟ':'tˤ',# CONSENSUS 1,2,3,4
-    #I worry about aproximant ð for the aligner
     'ⴷ':'d', # CONSENSUS 1,2,3,4
     'ⴹ':'dˤ',# CONSENSUS 1,2,3,4
+    
     'ⵍ':'l', # CONSENSUS 1,2,3,4
     'ⵔ':'r', # CONSENSUS 1,2,3,4
     'ⵕ':'rˤ',# CONSENSUS 1,2,3,4
@@ -125,26 +133,30 @@ tifinagh2ipa_dict = {
     # Palatal 
     'ⵐ':'ny',# SRC 2
 
-
     # Velar 		
     'ⵆ':'x',# SRC 2
     'ⴿ':'x',# IRCAM EXTENDED fricative; SRC 1 
     #'ⵅ':'χ',# SRC 1 CONFLICT!
     'ⵅ':'x',# SRC 2,3,4 CONFLICT!
+    'ⵅⵯ':'xʷ',# SRC 2,3,4 CONFLICT!
     'ⵖ':'ɣ',# CONSENSUS 1,2,3,4
+    'ⵖⵯ':'ɣʷ',# SRC 6; velarization
     'ⵗ':'ɣ',# SRC 2
 
     'ⴳ':'g',# CONSENSUS 1,2,3,4
     'ⴴ':'g',# SRC 2 (SRC 5 points to this being aproximant);   CONFLICT!
     #'ⴴ':'ʝ',# IRCAM EXTENDED fricative;SRC 1 CONFLICT!
     'ⴳⵯ':'ɡʷ',# CONSENSUS 1,2,3,4
+    # SRC 6; velarization
     'ⴽ':'k',# CONSENSUS 1,2,3,4
     'ⴾ':'k',# SRC 2
     'ⴿ':'k',# SRC 2 
     'ⴽⵯ':'kʷ',# CONSENSUS 1,2,3,4
+    # SRC 6; velarization
 
     # Uvular
     'ⵇ':'q',# CONSENSUS 1,2,3,4
+    'ⵇⵯ':'qʷ',# SRC 6;velarization
     'ⵈ':'q',# SRC 2
 
     # Pharyngeal
@@ -166,6 +178,9 @@ for k in tifinagh2ipa_dict:
 
 
 def tifinagh2ipa(text):
+    # Consider supporting diphtongs 
+    # /ts/ and /dz/
+    # /d͡ʒ/ and /t͡ʃ/
     orig = []
     trans = []
     i = 0
@@ -180,30 +195,34 @@ def tifinagh2ipa(text):
         i += 1
     return [orig,trans]
 
-# https://huggingface.co/datasets/TutlaytAI/tamazight_asr/viewer/default/train?p=1
 # https://en.wikipedia.org/wiki/Berber_Latin_alphabet
-latin2ipadict = {
+# and IRCAM Tifinagh~latin~arabic alphabet equivalence   
+latin2ipadict = { 
     ### VOWELS AND GLIDES
-    'a':'a',# or 'æ' depending on context
+    # https://en.wikipedia.org/wiki/Shilha_language#Vowels
+    'a':'a',# according to wiki is 'æ'
     'i':'i',
     'u':'u',# according to wiki is 'ʊ'
-    'e':'e',# according to wiki is 'ə'
+    'e':'e',# in normal Berber is 'ə' but not in Tachelhit
+    #https://en.wikipedia.org/wiki/Berber_Latin_alphabet#Souss-Berber_local_usage
     'ɛ':'ɛ',#according to wiki 'ʕ',
+    'â':'ɛ',#according to wiki 'ʕ',
     'w':'w',
     'y':'j', # and 'j':'ʒ'
 
     # Bilabials
     'm':'m',
+    'mʷ':'mʷ',
+    'mᵒ':'mᵒ',
     'b':'b',# or 'β'
-    #'p':'p',
+    'bʷ':'bʷ',
+    'bᵒ':'bᵒ',
 
     # Labiodental
     'f':'f',
-    'v':'v',
 
     # Dental
 
- 
     # Alveolar
     'n':'n',
 
@@ -213,35 +232,49 @@ latin2ipadict = {
     'ẓ':'zˤ',
 
     't':'t',# or 'θ' 
+    'ţ':'t͡s',
     'ṭ':'tˤ',
     'd':'d',# or 'ð' 
     'ḍ':'ðˤ',
+    'z̧':'d͡z',
 
     'l':'l', # or 'ɫ'
     'r':'r', # or 'rˤ'
     'ṛ':'rˤ',# 
-    'ř':'ɺ',# 
+    'ř':'ɺ',# between r and l, Rif Berber
+    # according to "Syllables in Tashlhiyt Berber and in Moroccan Arabic"
+    # by FRANÇOIS DELL, MOHAMED ELMEDLAOUI
+    # r should be IPA ɾ or r depending on context
 
     # Post Alveolar
     'c':'ʃ',
     'č':'t͡ʃ',
     'j':'ʒ',# and 'y':'j' #CONFLICT WITH TIFINAGH2IPA
+    'dj':'d͡ʒ',
+    #'ll':'d͡ʒ',# Rif-BERBER Only
     'ǧ':'d͡ʒ',
+    'ǧǧ':'d͡ʒ',
 
     # Palatal 
-    'ⵐ':'ny',# SRC 2
-
 
     # Velar 		
     'x':'x',# or 'χ' 
+    'xʷ':'xʷ',
+    'xᵒ':'xᵒ',
     'ɣ':'ɣ',# or 'ʁ'
+    'ɣʷ':'ɣʷ',
+    'ɣᵒ':'ɣᵒ',
     'g':'g',
-    'ɡʷ':'ɡʷ',# not in wiki
+    'ɡʷ':'ɡʷ',
+    'ɡᵒ':'ɡᵒ',
     'k':'k',
-    'kʷ':'kʷ',# not in wiki
+    'kʷ':'kʷ',
+    'kᵒ':'kᵒ',
 
     # Uvular
     'q':'q',# or 'qʷ' or 'ɢ'
+    'qʷ':'qʷ',
+    'qᵒ':'qᵒ',
  
     # Pharyngeal
     'ḥ':'ħ',# CONSENSUS 1,2,3
@@ -251,49 +284,110 @@ latin2ipadict = {
     
     # My annotations with 
     # https://huggingface.co/datasets/TutlaytAI/tamazight_asr
-    # Dental
-    # 't':'θ',
-    # Alveolar
+    #'t':'θ',
     #'ṭ':'t',
     #'s':'s',
     #'tt':'ts',
     #'z':'z',
     #'ṭṭ':'tˤ',
-    #'d':'d',# Can also be approximated ð
-    #'ḍ':'dˤ',
-    #'ẓ':'z',
-    # Post Alveolar
-    #'c':'ʃ',
-    #'čč':'',
-    # Palatal 
     #'y':'j',
-    # Uvular
     #'x':'χ',
-    # Pharyngeal
-    # Glottal 
-    #'ḥ':'hˤ',
     #'ʕ':'ʕ',
     #'ṛ':'rˤ',
     #'ṣ':'sˤ',
     #'ẓ':'zˤ',
-    #using writing from "Syllables in Tashlhiyt Berber and in Moroccan Arabic"
-    # by FRANÇOIS DELL, MOHAMED ELMEDLAOUI
-    #'š':'ʃ',
-    #'ž':'ʒ',
-    # Other uncommon symbols from IPA seem to already be ok in the datasets I have seen so far  
-    # Ambiguity between the book and IPA for symbols that appear in both IPA and latinscript berber:
-    # x,ɣ  has the IPA realizations χ and ʁ 
-    # Neotifinagh has 'ⵅ':'χ' ,'ⵖ':'ɣ',
-    # y should be IPA j 
-    #'y':'j',
-    #'w':'w',
-    # r should be IPA ɾ or r depending on context
-    # ! could mean next consonant is emphasized
-    # w and j are glides
 }
 
 
 def latins2ipa(text):
+    orig = []
+    trans = []
+    i = 0
+    while i< len(text):
+        if text[i] :
+            pass
+    return [orig,trans]
+
+# According to:
+# https://en.wikipedia.org/wiki/Berber_Latin_alphabet
+arabic2ipadict = {
+    ### VOWELS AND GLIDES
+    'ا':'a',# according to wiki is 'æ'
+    'أ':'a',# according to wiki is 'æ'
+    ' َ':'a',# according to wiki is 'æ'
+    'ي':'i',
+    ' ِ':'i',
+    'و':'u',# according to wiki is 'ʊ'
+    ' ُ':'u',# according to wiki is 'ʊ'
+    'ۍ':'e',# according to wiki is 'ə'
+    'ـ':'e',# according to wiki is 'ə'
+    'ع':'ɛ',# according to wiki 'ʕ',
+    'و':'w',
+    'ي':'j', # and 'j':'ʒ'
+
+    # Bilabials
+    'م':'m',
+    'ب':'b',# or 'β'
+    #'p':'p',
+
+    # Labiodental
+    'ف':'f',
+    #'v':'v',
+
+    # Dental
+
+    # Alveolar
+    'ن':'n',
+
+    'س':'s',
+    'ص':'sˤ',
+    'ز':'z',
+    'ژ':'zˤ',
+
+    'ث':'t',# or 'θ' 
+    'ت':'t',# or 'θ' 
+    'ط':'tˤ',
+    'ذ':'d',# or 'ð' 
+    'د':'d',# or 'ð' 
+    'ظ':'ðˤ',
+    'ض':'ðˤ',
+
+    'ل':'l', # or 'ɫ'
+    'ر':'r', # or 'rˤ'
+    'ر':'ɺ',# 
+    'ڕ':'rˤ',# 
+
+    # Post Alveolar
+    'ش':'ʃ',
+    'ت':'t͡ʃ',
+    'چ':'t͡ʃ',
+    'ج':'ʒ',# and 'y':'j' #CONFLICT WITH TIFINAGH2IPA
+    #'ج':'d͡ʒ', #ambiguity here.
+
+    # Palatal 
+
+    # Velar 		
+    'خ':'x',# or 'χ' 
+    'غ':'ɣ',# or 'ʁ'
+    'گ':'g',
+    'ݣ':'g',
+    #'ɡʷ':'ɡʷ',# North-berber, no proper writing in arabic
+    'ک':'k',
+    #'kʷ':'kʷ',#  North-berber, no proper writing in arabic
+
+    # Uvular
+    'ق':'q',# or 'qʷ' or 'ɢ'
+ 
+    # Pharyngeal
+    'ح':'ħ',# CONSENSUS 1,2,3
+
+    # Glottal 
+    'ه':'h',
+
+}
+
+
+def arabic2ipa(text):
     # TO DO 
     # Consider GEMMINATES: jj 
     orig = []
@@ -304,10 +398,6 @@ def latins2ipa(text):
             pass
     return [orig,trans]
 
-# https://www.livelingua.com/peace-corps/Tamazight/Tamazight%20Textbook%202007.pdf
-# This needs heavy reviewing
-# This is probably a better source: 
-# https://en.wikipedia.org/wiki/Help:IPA/Arabic
 
 def main():
     utils.prepare_project_structure()
