106ae6ebc3b781be8b2297ca1a986b23434c31c3
xarxaxdev <xarxax.dev@gmail.com>
2026-04-09 13:13:55 +0200

    finished doing all realizations

--- zgh_build_dicts.py
+++ zgh_build_dicts.py
@@ -17,7 +17,7 @@ In this script, 2 types of dictionary are created:
 Transliteration relevant links:
 - (Phon1) https://en.wikipedia.org/wiki/Shilha_language#Phonology
 - (Phon2) https://en.wikipedia.org/wiki/Central_Atlas_Tamazight#Phonology
-- (Phon3) https://en.wikipedia.org/wiki/Tarifit#Phonology
+- (Phon3) https://en.wikipedia.org/wiki/Tarifit#Phonology (An Introduction To Tarifiyt Berber)
 - (Phon4) https://en.wikipedia.org/wiki/Kabyle_language#Phonology
 
 Note that:
@@ -64,6 +64,7 @@ https://medium.com/@evan.frank/accessing-and-cleaning-bulk-wikipedia-text-data-b
 - Some are just specific realizations of existing phonemes(e.g. ⴲ is β). I will transform them to their specific realization (so long they are not in the corpus common_voice_22_0/zgh) for the sake of completion.
 - In case a symbol is in common_voice_22_0 I will consider it a standard symbol and adjust it to the best fitting phoneme in Phon[1-4]
 - A glottal stop ʔ is mentioned in the rif literature. Upon inspection on the sources, it has not dedicated Tifinagh symbol and in latin writing a ' is used. While ' appears in the data, seeing as ʔ is not part tzm,shi or even kab'; we will filter it out.
+- mʷ is exclusively mentioned in https://en.wikipedia.org/wiki/Berber_Latin_alphabet#Berber_Latin_alphabet_and_the_Tifinagh_Berber_alphabet ;  probably non-existant in tzm/shi/rif but present in some other Berber variant
 
 """
 tifinagh2ipa = {
@@ -478,12 +479,8 @@ ipa2latin = {
 
 # Multiple allophones, adding every option from Phon[1-3] so the aligner can choose the best match.
 # Ignoring gemminates as of now
-# RIF Vowels [Phon3]
-# https://www.universiteitleiden.nl/en/research/research-output/humanities/an-introduction-to-tarifiyt-berber
-# i-> i,ɪ,ɪˤ
-# a-> a,æ,ɑˤ
-# u-> u,ʊ,ʊˤ
-# vocalized r replacements:
+
+# RIF vocalized r replacements: [Phon3] -> IGNORED
 # iɾ -> ɛa
 # uɾ -> ɔa
 # aɾ -> a/æ
@@ -495,55 +492,88 @@ ipa2latin = {
 # shi dentalizes 
 # tzm/rif dont 
 
-# LABIALIZATION: contrastive
+# LABIALIZATION: contrastive (according to Phon2)
 # rif: kʷ,gʷ
 # tzm: xʷ,ɣʷ,qʷ,χʷ,ʁʷ
 # shi: kʷ,gʷ,qʷ,χʷ,ʁʷ
 
-# PHARINGEALIZATION: contrastive
+# PHARINGEALIZATION: contrastive (according to Phon2)
 # rif: dˤ,zˤ,rˤ,ʃˤ
 # tzm: tˤ,dˤ,sˤ,zˤ,lˤ,nˤ,rˤ
 # shi: tˤ,dˤ,sˤ,zˤ,lˤ,rˤ
 
+# SPIRANTIZATION: existing but non-contrastive
+# rif:  explicitly mentioned https://books.google.de/books?id=ZDxrzQEACAAJ&redir_esc=y
+#       both b and β have the same orthography
+
 # Generally a huge amount of vowel realizations due to Berber's limited vowel set
 # shi does not allow hiats, therefore : i->j/u->w/a->ʕ
 # conversely, between consonants: j->i/w->u
+# reference for this is: Syllables in Tashlhiyt Berber and in Moroccan Arabic
+# by FRANÇOIS DELL, MOHAMED ELMEDLAOUI
 
 # I used Phon[1-3] for each languages' specific realizations
 ipa2realization = {
     ### VOWELS AND GLIDES
-    'e':['e','ə','ɪ̈',''],# transitional vocoid
+    'e':['','ə','ɪ̈'],# transitional vocoid
     # shi: ''  from wiki "a vowel may be heard"; meaning not always
     # tzm: ɪ̈,ə
+    # rif: '' 
 
-    'a':['a','æ','ɐ','ʕ','ɑˤ'],
-    # shi: a,æ,ɐ,ʕ
+    'a':['a','æ','ɐ','ʕ','ɑ','ɑˤ'],
+    # shi: a,æ,ɐ,ʕ 
+    # tzm: æ,ɐ,ɑ
+    # rif: a,æ,ɑˤ (ɑˤ in vicinity of pharyngealized cons.)
 
-    'i':['i','ɪ','j','ɨ','e','ɪj','ɪˤ','ʝ','ɪʝ'],
-    # shi: i,ɪ,j
 
-    'u':['u','ɤ','w','ʊ','o','ʊw','wʊ''ʊˤ'],
+    'i':['i','ɪ','j','ɨ','e','ɪj','ɪˤ'],
+    # shi: i,ɪ,j 
+    # tzm: i,ɨ,ɪ,e,ɪj (ɪj as absolute end)
+    # rif: i,ɪ,ɪˤ (ɪˤ in vicinity of pharyngealized cons.)
+
+    'u':['u','ɤ','w','ʊ','o','ʊw','ʊˤ'],
     # shi: u,ɤ,w,ʊ
-    
-    'j':['j','i','ɪ','ʝ'],
-    #[j] is [i] between consonants
-    
-    'w':['w','u','ʊ'],
-    #[w] is [u] between consonants
+    # tzm: u,ʊ,o,ʊw  (ʊw as absolute end)
+    # rif: u,ʊ,ʊˤ (ʊˤ in vicinity of pharyngealized cons.)
+
+    'j':['j','i','ʝ'],
+    # shi: j,i (i between consonants)
+    # tzm: j
+    # rif: j,ʝ (ʝ has in most central rifian become j)
+
+    'w':['w','u'],
+    # shi: w,u (u between consonants)
+    # tzm: w
+    # rif: w
 
 
     # Bilabials
-    'b':['b','β'], # f from  https://en.wikipedia.org/wiki/Tarifit#Assimilations
+    'b':['b','β'], 
     'bʷ':['bʷ','βʷ'],
-    'p':['p','pʷ'],# rif only, may be pharyngealized
+    # shi: bʷ occurs sporadically in loandwords
+    # tzm: b spirantizes to β
+
+    'p':['p','pˤ'],# 
+    # shi: p occurs sporadically in loandwords
+    # tzm: p explicitly lacking
+    # rif: p may be pharyngealized(as a result of spreading)
+
     'm':'m',
-    'mʷ':'mʷ',
+    'mʷ':'mʷ', #non-existant in shi/tzm/rif
 
     # Labiodental
     'f':'f',
 
     # Alveolar
-    'n':['n','nˤ','n̪','n̪ˤ','ŋ'],# rif has ŋ as assimilation
+    'n':['n','nˤ','n̪','ŋ'],
+    # shi: n̪
+    # tzm: n,nˤ
+    # rif: n,ŋ assimilation exclusively before /w/
+
+    # CONSONANT SET: t,d,s,z,l,r
+    # all: have a contrastive pharyngealized equivalent (s-sˤ)
+    # shi: t,d,s,z,l,r are dental t̪,d̪,s̪,z̪,l̪,r̪
+    # tzm/rif: t,d,s,z,l,r are alveolar
 
     's':['s','s̪'],
     'sˤ':['sˤ','s̪ˤ'],
@@ -552,24 +582,32 @@ ipa2realization = {
 
     't':['t','θ','tʰ','t̪'],
     'tˤ':['tˤ','θˤ','t̪ˤ','θˤ'],
+    # tzm: t spirantizes to θ
     #'t͡s':'t͡s',
     'ts':'ts',
     'd':['d','ð','d̪'],
     'dˤ':['dˤ','ðˤ','d̪ˤ'],
+    # tzm: d spirantizes to ð
     #'d͡z':'d͡z',
     'dz':'dz',
 
-    'l':['l','ɫ'],
-    'r':['r','r̪','ɾ','r̪','rˤ'],
-    'rˤ':['ṛ','r̪'],
-    'ɺ':'ɺ',# Rif Berber only
-
-    # by FRANÇOIS DELL, MOHAMED ELMEDLAOUI
+    'l':'l',
+    'r':['r','r̪','ɾ','ɾ̪'],
+    # shi: r̪,ɾ̪
+    # tzm: r,ɾ
+    # rif: r,ɾ
+    'rˤ':['rˤ','r̪ˤ'],
+    #'ɺ':'ɺ',# Rif Berber only, written ř
+    # "Syllables in Tashlhiyt Berber and in Moroccan Arabic"by FRANÇOIS DELL, MOHAMED ELMEDLAOUI
     # r should be IPA ɾ or r depending on context
 
     # Post Alveolar
-    'ʃ':['ʃ','ç'],
-    'ʃˤ':'ʃˤ',#rif only
+    'ʃ':['ʃ','ʃˤ'],
+    # rif:  ʃ may be pharyngealized ʃˤ(as a result of spreading)
+    #       /ç/ has mostly become /ʃ/ in Central Riffian 
+    #       Note: I see no explicit writing of /ç/, so I presume it
+    #       uses the same orthography c.
+
     #'t͡ʃ':'t͡ʃ',
     'tʃ':'tʃ',
     'ʒ':'ʒ',
@@ -582,23 +620,36 @@ ipa2realization = {
     'ɡʷ':'ɡʷ',
     'k':'k',
     'kʷ':'kʷ',
+    # ASK AREA EXPERT
+    # shi/rif: have labialized g/k
+    # tzm: does not have labialized g/k
 
     # Uvular
-    #https://en.wikipedia.org/wiki/Central_Atlas_Tamazight#Phonology 
-    # "/χʷ/ and /ʁʷ/ rare—native speakers can freely substitute /χ ʁ/"
+    # shi:  clearly contrasts χʷ/χ and ʁʷ/ʁ
+    # tzm:  /χʷ/ and /ʁʷ/ rare(native speakers can freely substitute /χʁ/), however labialization is supposedly contrastive
+    #       x/ɣ can be a realization of k/g in specific dialect
+    # rif: no labialization; x,ɣ orthography should be x,ɣ (which is equivalent to χ,ʁ); ɣ gemminate is ʁ;
     'χ':['χ','x'],
-    'χʷ':['χʷ','xʷ','χ'],
-    'ʁ':['ʁ','ɣ'],# or 'ʁ'
-    'ʁʷ':['ʁʷ','ɣʷ','ʁ'],
-    'q':'q',# or 'qʷ' or 'ɢ'
+    'χʷ':['χʷ','xʷ'],
+    'ʁ':['ʁ','ɣ'],
+    'ʁʷ':['ʁʷ','ɣʷ'],
+
+    'q':'q',
     'qʷ':'qʷ',
+    # rif: qʷ is not mentioned
  
     # Pharyngeal
     'ħ':['ʜ','ħ'],# CONSENSUS 1,2,3
+    # shi: ʜ
+    # tzm,rif: ħ
     'ʕ':['ʢ','ʕ'],
+    # shi: ʢ
+    # tzm,rif: ʕ
 
     # Glottal 
     'h':['h','ɦ'],
+    # shi,rif: ɦ
+    # tzm: h
     
     #clitics
     '-':'-',
